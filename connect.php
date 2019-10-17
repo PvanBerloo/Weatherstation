2b56410ad2300c3e2241ba071a40fe50543d6b67
PvanBerloo <pa.vanberloo@student.avans.nl>
2019-10-17 14:41:17 +0200

    Add php code

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "**";
+$username = "**";
+$password = "**";
+$database = "**";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully\n";
+
+date_default_timezone_set("Europe/Amsterdam");
+$date = date("Y-m-d");
+$time = date("G:i:s");
+
+$temperature=intval($_GET["temperature"])/100;
+$humidity=intval($_GET["humidity"])/100;
+$pressure=intval($_GET["pressure"])/100;
+
+$conn->query("INSERT INTO data (temperature, humidity, pressure, date, time) VALUES (".$temperature.", ".$humidity.", ".$pressure.", '".$date."', '".$time."')")
+?>
